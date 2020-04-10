2cd1f1edc0bd58cc559d4cd48da8c3b899dbd76e
Luís Henrique Machado <lhfam97@gmail.com>
2020-04-10 12:53:16 -0300

    First commit exercise NODE

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,58 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+  const project = {id:uuid(),title,url,techs,likes:0}
+  repositories.push(project);
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const {title,url,techs} = request.body;
+  const repository_id = repositories.findIndex(repository => repository.id === id);
+  console.log(repository_id);
+  if(repository_id>=0){
+    repositories[repository_id].techs = techs;
+    repositories[repository_id].title = title;
+    repositories[repository_id].url = url;
+  return response.json(repositories[repository_id])
+}
+else{
+  return response.status(400).json({error: "Directory not found"});
+}
+
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const repository_id = repositories.findIndex(repository => repository.id === id);
+  console.log(id);
+  if(repository_id>=0){ 
+   repositories.splice(repository_id,1);
+    return response.status(204).send()
+  }
+  else{
+    return response.status(400).json({error: "Directory not found"});
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const repository_id = repositories.findIndex(repository => repository.id === id);
+  if(repository_id >= 0){
+    repositories[repository_id].likes++;
+    return response.status(200).json(repositories[repository_id])
+  }
+  else{
+    return response.status(400).json({error: "Directory not found"});
+  }
+  
 });
 
 module.exports = app;
